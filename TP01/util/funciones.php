60b148d38784062051a6b01a941e2bea362760c2
Franku88 <fabian.benitez@est.fi.uncoma.edu.ar>
2024-08-17 02:38:38 -0300

    commit 2024-08-17

--- TP01/util/funciones.php
+++ TP01/util/funciones.php
@@ -1,7 +1,41 @@
 <?php 
-
 //Implementar funcion para obtener info de formularios
+function data_submitted() 
+{
+	$data = array();
+    if (!empty($_POST)) { //Chequea que metodo se utilizo
+    	$data = $_POST;
+    } else {
+		if(!empty($_GET)) { 
+            $data = $_GET;
+		}
+    }
+	if (count($data) != 0) {
+		foreach ($data as $indice => $valor) {
+			if ($valor=="") {
+				$data[$indice] = null;
+			}
+		}
+	}
+	return $data;
+}
 
-
-
+spl_autoload_register(
+	function ($clase) 
+	{
+		echo "Cargamos la clase  ".$clase."<br>" ;
+		$directorys = array(
+			$GLOBALS['ROOT'].'modelo/',
+			$GLOBALS['ROOT'].'control/',
+		);
+		//print_r($directorys) ;
+		foreach($directorys as $directory){
+		if(file_exists($directory.$clase . '.php')){  
+				// echo "se incluyo".$directory.$class_name . '.php';
+				require_once($directory.$clase . '.php');
+				return;
+			}           
+		}
+	}
+);
 ?>
---
+++ TP01/vista/ej7.php
@@ -0,0 +1,7 @@
+Ejercicio 7
+Crear una página con un formulario que contenga dos input de tipo text y un select. En
+los inputs se ingresarán números y el select debe dar la opción de una operación
+matemática que podrá resolverse usando los números ingresados. En la página que
+procesa la información se debe mostrar por pantalla la operación seleccionada, cada
+uno de los operandos y el resultado obtenido de resolver la operación. Ejemplo del
+formulario:
