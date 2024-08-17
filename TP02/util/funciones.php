1ebaf7eebd9aed13163ad85f3e4543279b8e91bc
Franku88 <fabian.benitez@est.fi.uncoma.edu.ar>
2024-08-17 20:41:58 -0300

    2024-08-17 agregando validaciones a ejs del tp1

--- TP02/util/funciones.php
+++ TP02/util/funciones.php
@@ -2,41 +2,40 @@
 //Implementar funcion para obtener info de formularios
 function data_submitted() 
 {
-	$data= array();
-    if (!empty($_POST)) {
-    	$data =$_POST;
+	$data = array();
+    if (!empty($_POST)) { //Chequea que metodo se utilizo
+    	$data = $_POST;
     } else {
-		if(!empty($_GET)) {
+		if(!empty($_GET)) { 
             $data = $_GET;
 		}
     }
-	if (count($data)) {
+	if (count($data) != 0) {
 		foreach ($data as $indice => $valor) {
-				if ($valor=="") {
-                	$data[$indice] = 'null'	;
-                }
+			if ($valor=="") {
+				$data[$indice] = null;
 			}
+		}
 	}
 	return $data;
 }
 
-
-spl_autoload_register(function ($clase) {
-	echo "Cargamos la clase  ".$clase."<br>" ;
-	$directorys = array(
-		$GLOBALS['ROOT'].'modelo/',
-		$GLOBALS['ROOT'].'control/',
-	);
-   // print_r($directorys) ;
-	foreach($directorys as $directory){
-	  if(file_exists($directory.$clase . '.php')){  
-			  // echo "se incluyo".$directory.$class_name . '.php';
-			require_once($directory.$clase . '.php');
-			return;
-		}           
+spl_autoload_register(
+	function ($clase) 
+	{
+		echo "Cargamos la clase  ".$clase."<br>" ;
+		$directorys = array(
+			$GLOBALS['ROOT'].'modelo/',
+			$GLOBALS['ROOT'].'control/',
+		);
+		//print_r($directorys) ;
+		foreach($directorys as $directory){
+		if(file_exists($directory.$clase . '.php')){  
+				// echo "se incluyo".$directory.$class_name . '.php';
+				require_once($directory.$clase . '.php');
+				return;
+			}           
+		}
 	}
-
-   
-});
-
+);
 ?>
