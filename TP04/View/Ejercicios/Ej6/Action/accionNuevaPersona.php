2190f9762f5b0f7aa0bf40a645a72204b3c1f95d
Franku88 <fabian.benitez@est.fi.uncoma.edu.ar>
2024-09-11 00:16:27 -0300

    2024-09-11 ej6 faltan validaciones de form

---
+++ TP04/View/Ejercicios/Ej6/Action/accionNuevaPersona.php
@@ -0,0 +1,49 @@
+<?php
+include_once '../../../../configuracion.php';
+include_once ROOT_PATH.'/Util/funciones.php';
+include_once ROOT_PATH.'/Controller/ABMPersona.php';
+include_once ROOT_PATH.'/View/Assets/Helper.php';
+
+$data = data_submitted();
+$resultado = "<div class='alert alert-warning border-steam-inactivo'>
+                <h5> Datos no recibidos.</h5>
+            </div>";
+
+if (!empty($data)) {
+    $arrPersona = (new ABMPersona())->buscar(['NroDni'=>($data['nrodni'])]);
+    if (empty($arrPersona)) {
+        $nuevaPersona = [
+            'NroDni'=> ($data['nrodni']),
+            'Apellido'=>($data['apellido']),
+            'Nombre'=>($data['nombre']),
+            'fechaNac'=>($data['fechanac']),
+            'Telefono'=>($data['codarea'].'-'.$data['numlocal']),
+            'Domicilio'=>($data['domicilio'])
+        ];
+        if((new ABMPersona())->alta($nuevaPersona)) {
+            $resultado = "<div class='alert alert-success border-steam-inactivo'>
+            <h5> Persona registrada con exito.</h5>
+            </div>";
+        } else {
+            $resultado = "<div class='alert alert-warning border-steam-inactivo'>
+            <h5> Alta no realizada, algún dato no válido en la BD.</h5>
+            </div>";
+        }
+    } else {
+        $resultado = "<div class='alert alert-warning border-steam-inactivo'>
+            <h5> Persona con DNI '".($data['nrodni'])."' ya se encuentra registrada.</h5>
+            </div>";
+    }
+}
+?>
+
+<?php include_once STRUCTURE_PATH.'/Head.php';?>
+ 
+    <div class="container mt-5">
+        <?php echo $resultado;?>
+        <div>
+            <a href="../NuevaPersona.php" class="btn btn-secondary btn-steam2"> Volver </a>
+        </div>
+    </div>
+
+<?php include_once STRUCTURE_PATH.'/Foot.php';?>
--- TP04/View/Ejercicios/Ej6/NuevaPersona.php
+++ TP04/View/Ejercicios/Ej6/NuevaPersona.php
@@ -4,6 +4,78 @@ include_once '../../../configuracion.php';
 
 <?php include_once STRUCTURE_PATH.'/Head.php';?>
 
-<h1> EN NUEVA PERSONA </h1>
+    <div class="modal-dialog m-auto">
+        <div class="modal-content rounded-4 shadow contenedor-inactivo-steam">
+            <div class="modal-header p-5 pb-4 border-bottom-0">
+                <h1 class="mb-0">Registrar Persona</h1>
+            </div>
+            <div class="modal-body p-5 pt-0">
+                <form id="formEj6" action="Action/accionNuevaPersona.php" method="POST">
+                    
+                    <div class="form-floating mb-3">
+                        <input type="text" class="form-control rounded-3" name="nrodni" id="nrodni" placeholder="">
+                        <label for="nrodni"> DNI </label>
+                        <div class="invalid-feedback">
+                        DNI de 8 digitos, sin puntos.
+                        </div>
+                    </div>
+
+                    <div class="form-floating mb-3">
+                        <input type="text" class="form-control rounded-3" name="apellido" id="apellido" placeholder="">
+                        <label for="apellido"> Apellido </label>
+                        <div class="invalid-feedback">
+                        Ingrese solo letras.
+                        </div>
+                    </div>
+
+                    <div class="form-floating mb-3">
+                        <input type="text" class="form-control rounded-3" name="nombre" id="nombre" placeholder="">
+                        <label for="nombre"> Nombre </label>
+                        <div class="invalid-feedback">
+                        Ingrese solo letras.
+                        </div>
+                    </div>
+
+                    <div class="form-floating mb-3">
+                        <input type="date" class="form-control rounded-3" name="fechanac" id="fechanac" placeholder="">
+                        <label for="fechanac"> Fecha de Nacimiento </label>
+                        <div class="invalid-feedback">
+                        Debe ser mayor de 18 años.
+                        </div>
+                    </div>
+
+                    <div class="d-flex">
+                        <div class="form-floating mb-3 me-1 w-25">
+                            <input type="text" class="form-control rounded-3" name="codarea" id="codarea" placeholder="" minlength="2" maxlength="4">
+                            <label for="codarea"> Cod. area </label>
+                            <div class="invalid-feedback">
+                            Deben ser 10 digitos entre Cod. area y Número.
+                            </div>
+                        </div>
+
+                        <div class="form-floating mb-3 ms-1 w-75">
+                            <input type="text" class="form-control rounded-3" name="numlocal" id="numlocal" placeholder="" minlength="6" maxlength="8">
+                            <label for="numlocal"> Número </label>
+                            <div class="invalid-feedback">
+                            Deben ser 10 digitos entre Cod. area y Número.
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="form-floating mb-3">
+                        <input type="text" class="form-control rounded-3" name="domicilio" id="domicilio" placeholder="">
+                        <label for="domicilio"> Domicilio </label>
+                        <div class="invalid-feedback">
+                        Domicilio no válido.
+                        </div>
+                    </div>
+
+                    <div class="form-floating mb-2">
+                        <button class="w-100 mb-2 btn btn-lg rounded-3 btn-primary btn-steam4" type="submit">Enviar</button>
+                    </div>
+                </form>
+            </div>
+        </div>
+    </div>
 
 <?php include_once STRUCTURE_PATH.'/Foot.php';?>
