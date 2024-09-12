093d02a79beb2e275249efc55338a32ab6d61a24
Franku88 <fabian.benitez@est.fi.uncoma.edu.ar>
2024-09-12 20:39:33 -0300

    separador agregado

--- TP04/View/Structure/Foot.php
+++ TP04/View/Structure/Foot.php
@@ -1,6 +1,7 @@
 <!-- Contiene scripts a utilizar, footer para todas las paginas, cierra body y html -->
 
-        <footer class="contenedor-inactivo-steam py-3 mt-auto">
+        <div class="mt-4"></div> <!-- Separador, evita que divs se peguen al footer-->
+        <footer class="contenedor-inactivo-steam py-3 mt-auto">    
             <div class="mx-4 d-flex flex-wrap justify-content-between align-items-center">
                 <div class="d-flex">
                     <span> &copy; Benitez, Franco Fabian [FAI-3169]; PWD 2024 </span>
