f6a17229a06ddb17674e06aae354588a7b4d6880
Franku88 <fabian.benitez@est.fi.uncoma.edu.ar>
2024-09-12 20:44:40 -0300

    enunciado agregado

--- TP04/View/Ejercicios/Ej6/NuevaPersona.php
+++ TP04/View/Ejercicios/Ej6/NuevaPersona.php
@@ -4,6 +4,14 @@ include_once '../../../configuracion.php';
 
 <?php include_once STRUCTURE_PATH.'/Head.php';?>
 
+    <div class="alert alert-secondary rounded-4 m-5 border-steam-activo">
+        <p class="fs-5"> <b>Ejercicio 6</b> - Crear una página “NuevaPersona.php” que contenga un formulario que permita solicitar todos
+        los datos de una persona. Estos datos serán enviados a una página “accionNuevaPersona.php” que cargue
+        un nuevo registro en la tabla persona de la base de datos. Se debe mostrar un mensaje que indique si se
+        pudo  o no  cargar  los datos  de  la persona.  Utilizar  css y  validaciones  javaScript cuando  crea  conveniente.
+        Recordar usar la capa de control antes generada, no se puede acceder directamente a las clases del ORM.</p>
+    </div>
+
     <div class="modal-dialog m-auto">
         <div class="modal-content rounded-4 shadow contenedor-inactivo-steam w-auto">
             <div class="modal-header p-5 pb-4 border-bottom-0">
