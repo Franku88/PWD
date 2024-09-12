86eba2bea8c5dbe224658b133a852ce3b2088ee2
Franku88 <fabian.benitez@est.fi.uncoma.edu.ar>
2024-09-12 20:18:30 -0300

    enunciado agregado

--- TP04/View/Ejercicios/Ej3/verAutos.php
+++ TP04/View/Ejercicios/Ej3/verAutos.php
@@ -5,9 +5,13 @@ include_once ROOT_PATH.'/View/Assets/Helper.php';
 
 $arrAutos = (new ABMAuto())->buscar();
 
-$resultado = "";
+$resultado = "<div class='alert alert-warning border-steam-inactivo'>
+                <h5> Datos no recibidos.</h5>
+            </div>";
 if (empty($arrAutos)) {
-    $resultado = "<h5> No se encuentran autos cargados en la BD.</h5>";
+    $resultado = "  <div class='alert alert-warning border-steam-inactivo'>
+                    <h5> No se encuentran autos cargados en la BD.</h5>
+                </div>";
 } else {
     $arreglo = [];
     foreach($arrAutos as $auto) {
@@ -18,7 +22,12 @@ if (empty($arrAutos)) {
 ?>
 
 <?php include STRUCTURE_PATH.'/Head.php';?>
-    
+    <div class="alert alert-secondary rounded-4 m-5 border-steam-activo">
+        <p class="fs-5"> <b>Ejercicio 3</b> - Crear   una   pagina   php   “VerAutos.php”,   en   ella   usando   la   capa   de   control   correspondiente
+        mostrar todos los datos de los autos que se encuentran cargados, de los dueños mostrar nombre y apellido.
+        En caso de que no se encuentre ningún auto cargado en la base mostrar un mensaje indicando que no hay
+        autos cargados.</p>
+    </div>
     <div class="mx-4 my-3">
         <h1> Autos Registrados </h1> 
         <div class="rounded-4 overflow-x-auto p-3"><?php echo($resultado);?></div>
