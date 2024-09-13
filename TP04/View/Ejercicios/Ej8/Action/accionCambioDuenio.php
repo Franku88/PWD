70b4c229e25572a5df45b8e0eb663f57a9107bd5
Franku88 <fabian.benitez@est.fi.uncoma.edu.ar>
2024-09-12 22:05:37 -0300

    2024-12-09 ej8 terminado

---
+++ TP04/View/Ejercicios/Ej8/Action/accionCambioDuenio.php
@@ -0,0 +1,67 @@
+<?php
+include_once '../../../../configuracion.php';
+include_once ROOT_PATH.'/Util/funciones.php';
+include_once ROOT_PATH.'/Controller/ABMPersona.php';
+include_once ROOT_PATH.'/Controller/ABMAuto.php';
+
+$data = data_submitted();
+
+$resultado = "<div class='alert alert-warning border-steam-inactivo'>
+                <h5> Datos no recibidos.</h5>
+            </div>";
+
+if (!empty($data)) {
+    $arrAuto = (new ABMAuto())->buscar(['Patente'=>(strtoupper($data['patente']))]);
+    if (!empty($arrAuto)) {
+        $arrPersona = (new ABMPersona())->buscar(['NroDni'=>($data['dniduenio'])]);
+        if (!empty($arrPersona)) {
+            if (($arrAuto[0]->getDuenio())->getNroDni() != ($data['dniduenio'])) { //Verifica que no sea duenio actualmente
+                $autoModificado = [
+                    'Patente'=> ($arrAuto[0]->getPatente()),
+                    'Marca'=> ($arrAuto[0]->getMarca()),
+                    'Modelo'=> ($arrAuto[0]->getModelo()),
+                    'Duenio'=> ($arrPersona[0])
+                ];
+                if ((new ABMAuto())->modificacion($autoModificado)) {
+                    $resultado = "<div class='alert alert-success border-steam-inactivo'>
+                    <h5> Dueño modificado con exito.</h5>
+                    </div>
+                    <div class='mb-1'>
+                        <a href='../../Ej3/verAutos.php' class='btn btn-secondary btn-steam2'> Ver autos </a>
+                    </div>";
+                } else {
+                    $resultado = "<div class='alert alert-warning border-steam-inactivo'>
+                    <h5> Alta no realizada, algún dato no válido en la BD.</h5>
+                    </div>";
+                }
+            } else {
+                $resultado = "<div class='alert alert-warning border-steam-inactivo'>
+                    <h5> Persona con DNI '".($data['dniduenio'])."' ya es dueña del auto con pantente '".strtoupper($data['patente'])."'.</h5>
+                    </div>
+                    <div class='mb-1'>
+                        <a href='../../Ej3/verAutos.php' class='btn btn-secondary btn-steam2'> Ver autos </a>
+                    </div>";
+            }
+        } else {
+            $resultado = "<div class='alert alert-warning border-steam-inactivo'>
+                <h5> Persona con DNI '".($data['dniduenio'])."' no se encuentra registrada.</h5>
+                </div>";
+        }
+    } else {
+        $resultado = "<div class='alert alert-warning border-steam-inactivo'>
+                <h5> Auto con pantente '".strtoupper($data['patente'])."' no se encuentra registrado.</h5>
+                </div>";
+    }
+}
+?>
+
+<?php include_once STRUCTURE_PATH.'/Head.php';?>
+ 
+    <div class="container mt-5">
+        <?php echo $resultado;?>
+        <div>
+            <a href="../CambioDuenio.php" class="btn btn-secondary btn-steam2"> Volver </a>
+        </div>
+    </div>
+
+<?php include_once STRUCTURE_PATH.'/Foot.php';?>
