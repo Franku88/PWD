fb399b7dd19890acd7b6ad7224b0bf4009799e96
Franku88 <fabian.benitez@est.fi.uncoma.edu.ar>
2024-09-13 17:43:32 -0300

    Info de grupo

--- TP04/index.php
+++ TP04/index.php
@@ -4,8 +4,20 @@
 ?>
 
 <!-- <body> ...-->
-        <div class="text-center p-2">
-            <h1> REGISTRO AUTOMOTOR </h1>
+        <div class="text-center p-2 contenedor-inactivo-steam mx-auto mt-5 rounded-4">
+            <h2 class="m-4">Programación Web Dinámica - Grupo 4</h2>
+            <div class="contenedor-activo-steam rounded-4 m-4">
+                <h4 class="m-4">[FAI-3169] Benitez, Franco Fabian<br><a href='mailto:fabian.benitez@est.fi.uncoma.edu.ar'>fabian.benitez@est.fi.uncoma.edu.ar</a></h4>
+            </div>
+            <div class="contenedor-activo-steam rounded-4 m-4">
+                <h4 class="m-4">[FAI-4572] Molina, Joaquin Gabriel<br><a href='mailto:joaquin.molina@est.fi.uncoma.edu.ar'>joaquin.molina@est.fi.uncoma.edu.ar</a></h4>
+            </div>
+            <div class="contenedor-activo-steam rounded-4 m-4">
+                <h4 class="m-4">[FAI-4594] Pesce, Matías Nicolás<br><a href='mailto:matias.pesce@est.fi.uncoma.edu.ar'>matias.pesce@est.fi.uncoma.edu.ar</a></h4>
+            </div>
+            <div class="contenedor-activo-steam rounded-4 m-4">
+                <h4 class="m-4">[FAI-3220] Reyes Castelló, José Vicente<br><a href='mailto:jose.reyes@est.fi.uncoma.edu.ar'>jose.reyes@est.fi.uncoma.edu.ar</a></h4>
+            </div>
         </div>
 <!--... </body>-->            
 
